a96e6e84da02fa21c17e4a81cc623f9acd0b9132
Rustam Mamatov <Mamatovr31@gmail.com>
2025-12-25 19:55:12 +0200

    Fix formatting in format_single_linter_file to satisfy flake8

--- app/main.py
+++ app/main.py
@@ -9,9 +9,16 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-        formatted_errors = [{"line": error["line_number"], "column": error["column_number"],
-        "message": error["text"], "name": error["code"], "source": "flake8"}
-        for error in errors]
+        formatted_errors = [
+        {
+            "line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"
+        }
+        for error in errors
+        ]
         return {
             "errors": formatted_errors, "path": file_path,
             "status": "passed" if not formatted_errors else "failed"
