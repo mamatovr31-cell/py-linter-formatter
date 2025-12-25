ddec95b8da457f0de4a9848f1f944054a67576b7
Rustam Mamatov <Mamatovr31@gmail.com>
2025-12-25 19:36:43 +0200

    remove  type annotations

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def format_linter_error(error: dict) -> dict:
-    def format_linter_error(error):
-        return {
+    return {
             "line": error["line_number"],
             "column": error["column_number"],
             "message": error["text"],
             "name": error["code"],
             "source": "flake8"
         }
+
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
         formatted_errors = [{"line": error["line_number"], "column": error["column_number"],
@@ -17,6 +17,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
             "status": "passed" if not formatted_errors else "failed"
         }
 
-def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path=path, errors=errs) for (path, errs) in linter_report.items()]
 
+def format_linter_report(linter_report: dict) -> list:
+    return [format_single_linter_file(file_path=path, errors=errs)
+            for (path, errs) in linter_report.items()]
